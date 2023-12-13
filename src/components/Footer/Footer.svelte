b09da1be433dca9541a6e59b9140742e0bf9dfb9
tvalenzu <tvalenzu@gmail.com>
2023-12-13 09:27:39 -0300

    elimina clear lab

--- src/components/Footer/Footer.svelte
+++ src/components/Footer/Footer.svelte
@@ -97,7 +97,7 @@
 		<div class="pt-4 md:flex md:items-center md:justify-center" style="border-top:1px solid white">
 			<ul class="">
 				<li class="md:mx-2 md:inline leading-7 text-sm" id="footer-navi-2">
-					<a class=" text-center underline text-small" href="/">ClearLab. Inc</a>
+					<a class=" text-center underline text-small" href="/"></a>
 				</li>
 			</ul>
 		</div>
