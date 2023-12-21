ef7bcd088b7b8ae4f41a1dbbaac1ae4a9ef9731a
tvalenzu <tvalenzu@gmail.com>
2023-12-21 19:47:23 -0300

    Update contactanos.svelte

--- src/components/Contactanos/contactanos.svelte
+++ src/components/Contactanos/contactanos.svelte
@@ -9,7 +9,7 @@
 		class="items-center justify-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4 rtl:space-x-reverse"
 	>
 		<a
-			href="https://ff.healthatom.io/BKRXlS"
+			href="https://ff.healthatom.io/0AxobO"
 			class="w-full sm:w-auto bg-green-600 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700"
 		>
 			<svg
